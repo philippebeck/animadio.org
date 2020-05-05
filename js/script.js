35c1046fbe7fb91601058e59f7f12e9e0d4bbecb
Philippe Beck <philippe@philippebeck.net>
2020-05-05 16:50:00 +0200

    Add {key} to demo Ids for animadio.js v0.1.1

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var animadio = new Animadio(
       ["name", "function", "count", "direction"],
+      "anima",
       [2000, {few: 2, many: 5, loop: 30}]
   );
 });
