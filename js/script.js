aed0ae3ab39826378e847fefaa2931dd3535ed17
Philippe Beck <philippe@philippebeck.net>
2020-05-07 18:11:51 +0200

    Refactoring for new structure with Input class

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
-  var animadio = new Animadio(
+  new Input(
       ["name", "function", "count", "direction"],
       ["anima", ["check", "hub", "goal"]],
       [2000, {few: 2, many: 5, loop: 30}]
