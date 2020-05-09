4d6380dc10e7624b0d7c22245aa73a790949846b
Philippe Beck <philippe@philippebeck.net>
2020-05-09 22:38:56 +0200

    Add Slider to script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
-  new Input(
+  let input = new Input(
       ["name", "function", "count", "direction"],
       ["anima", ["check", "hub", "goal"]],
       [2000, {few: 2, many: 5, loop: 30}]
   );
+
+  let slider = new Slider ();
 });
