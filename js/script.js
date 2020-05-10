8df5bc1e3aad0d09b2bdb2a1d5c8f6915db729f3
Philippe Beck <philippe@philippebeck.net>
2020-05-10 19:30:21 +0200

    Update calls to animadio.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
-  let input = new Input(
+  Animadio.input(
       ["name", "function", "count", "direction"],
       ["anima", ["check", "hub", "goal"]],
       [2000, {few: 2, many: 5, loop: 30}]
   );
 
-  let slider = new Slider ();
+  Animadio.slider();
 });
