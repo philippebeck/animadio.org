401175049e3f05b7cc4db08e453a1b12b94bfaaa
Philippe Beck <philippe@philippebeck.net>
2021-03-05 21:48:30 +0100

    Switch index to dev mode

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 
 use Pam\Controller\FrontController;
-//use Tracy\Debugger;
+use Tracy\Debugger;
 
 require_once '../vendor/autoload.php';
-require_once '../config/prod-params.php';
+require_once '../config/dev-params.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-//Debugger::enable();
+Debugger::enable();
 
 $frontController = new FrontController();
 $frontController->run();
