dbb4af5ffd29871365b0f41e2e9542d754dbaf40
Philippe Beck <philippe@philippebeck.net>
2021-03-18 22:02:10 +0100

    Create new element controller & view

---
+++ src/Controller/ElementController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Controller\MainController;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class ElementController
+ * @package App\Controller
+ */
+class ElementController extends MainController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        return $this->render("front/element.twig");
+    }
+}
