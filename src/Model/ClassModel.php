d7a2ca904b97e6bfaf9b47bfb8986ba1f220dc94
Philippe Beck <philippe@philippebeck.net>
2021-03-23 14:19:01 +0100

    Crate listClassesWithCategory() to ClassModel

--- src/Model/ClassModel.php
+++ src/Model/ClassModel.php
@@ -8,4 +8,15 @@ use Pam\Model\MainModel;
  * Class ClassModel
  * @package App\Model
  */
-class ClassModel extends MainModel {}
+class ClassModel extends MainModel 
+{
+    public function listClassesWithCategory()
+    {
+        $query = "SELECT * FROM Class
+            INNER JOIN Category 
+            ON Class.category_id = Category.id
+            ORDER BY Category.id";
+    
+        return $this->database->getAllData($query);
+    }
+}
