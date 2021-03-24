0d731c881e266e8eb74a5ae45f15ff28b653e9e8
Philippe Beck <philippe@philippebeck.net>
2021-03-24 15:22:43 +0100

    Create new Models

---
+++ src/Model/TemplateModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Pam\Model\MainModel;
+
+/**
+ * Class TemplateModel
+ * @package App\Model
+ */
+class TemplateModel extends MainModel {}
---
+++ src/Model/ThemeModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Pam\Model\MainModel;
+
+/**
+ * Class ThemeModel
+ * @package App\Model
+ */
+class ThemeModel extends MainModel {}
