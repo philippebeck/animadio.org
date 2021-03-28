9d481ff1b57a18e0e173aa7b23a0c01f873fede9
Philippe Beck <philippe@philippebeck.net>
2021-03-28 18:17:58 +0200

    Add CRUD to Element, Template & Theme Controllers

--- src/Controller/ElementController.php
+++ src/Controller/ElementController.php
@@ -15,6 +15,11 @@ use Twig\Error\SyntaxError;
 class ElementController extends MainController
 {
     /**
+     * @var array
+     */
+    private $element = [];
+
+    /**
      * @return string
      * @throws LoaderError
      * @throws RuntimeError
@@ -27,5 +32,89 @@ class ElementController extends MainController
         return $this->render("front/element.twig", [
             "elements" => $elements
         ]);
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function createMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        if (!empty($this->getPost()->getPostArray())) {
+            $this->setElementData();
+            $this->setElementImage();
+
+            ModelFactory::getModel("Element")->createData($this->element);
+            $this->getSession()->createAlert("New element successfully created !", "green");
+
+            $this->redirect("admin");
+        }
+
+        return $this->render("back/element/createElement.twig");
+    }
+
+    private function setElementData()
+    {
+        $this->element["name"]          = (string) trim($this->getPost()->getPostVar("name"));
+        $this->element["definition"]    = (string) trim($this->getPost()->getPostVar("definition"));
+
+        $this->element["link"]  = (string) trim($this->getPost()->getPostVar("link"));
+        $this->element["link"]  = str_replace("https://codepen.io/animadio/pen/", "", $this->element["link"]);
+    }
+
+    private function setElementImage()
+    {
+        $this->getFiles()->uploadFile("img/elements/", $this->element["name"] . ".png");
+        $this->getImage()->makeThumbnail("img/elements/" . $this->element["name"] . ".png", 600);
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function updateMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        if (!empty($this->getPost()->getPostArray())) {
+            $this->setElementData();
+
+            if (!empty($this->getFiles()->getFileVar("name"))) {
+                $this->setElementImage();
+            }
+
+            ModelFactory::getModel("Element")->updateData($this->getGet()->getGetVar("id"), $this->element);
+            $this->getSession()->createAlert("Successful modification of the element !", "blue");
+
+            $this->redirect("admin");
+        }
+
+        $element = ModelFactory::getModel("Element")->readData($this->getGet()->getGetVar("id"));
+
+        return $this->render("back/element/updateElement.twig", [
+            "element" => $element
+        ]);
+    }
+
+    public function deleteMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        ModelFactory::getModel("Element")->deleteData($this->getGet()->getGetVar("id"));
+        $this->getSession()->createAlert("Element actually deleted !", "red");
+
+        $this->redirect("admin");
     }
 }
--- src/Controller/TemplateController.php
+++ src/Controller/TemplateController.php
@@ -15,6 +15,11 @@ use Twig\Error\SyntaxError;
 class TemplateController extends MainController
 {
     /**
+     * @var array
+     */
+    private $template = [];
+
+    /**
      * @return string
      * @throws LoaderError
      * @throws RuntimeError
@@ -27,5 +32,89 @@ class TemplateController extends MainController
         return $this->render("front/template.twig", [
             "templates" => $templates
         ]);
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function createMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        if (!empty($this->getPost()->getPostArray())) {
+            $this->setTemplateData();
+            $this->setTemplateImage();
+
+            ModelFactory::getModel("Template")->createData($this->template);
+            $this->getSession()->createAlert("New template successfully created !", "green");
+
+            $this->redirect("admin");
+        }
+
+        return $this->render("back/template/createTemplate.twig");
+    }
+
+    private function setTemplateData()
+    {
+        $this->template["name"]          = (string) trim($this->getPost()->getPostVar("name"));
+        $this->template["definition"]    = (string) trim($this->getPost()->getPostVar("definition"));
+
+        $this->template["link"]  = (string) trim($this->getPost()->getPostVar("link"));
+        $this->template["link"]  = str_replace("https://codepen.io/animadio/pen/", "", $this->template["link"]);
+    }
+
+    private function setTemplateImage()
+    {
+        $this->getFiles()->uploadFile("img/templates/", $this->template["name"] . ".png");
+        $this->getImage()->makeThumbnail("img/templates/" . $this->template["name"] . ".png", 600);
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function updateMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        if (!empty($this->getPost()->getPostArray())) {
+            $this->setTemplateData();
+
+            if (!empty($this->getFiles()->getFileVar("name"))) {
+                $this->setTemplateImage();
+            }
+
+            ModelFactory::getModel("Template")->updateData($this->getGet()->getGetVar("id"), $this->template);
+            $this->getSession()->createAlert("Successful modification of the template !", "blue");
+
+            $this->redirect("admin");
+        }
+
+        $template = ModelFactory::getModel("Template")->readData($this->getGet()->getGetVar("id"));
+
+        return $this->render("back/template/updateTemplate.twig", [
+            "template" => $template
+        ]);
+    }
+
+    public function deleteMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        ModelFactory::getModel("Template")->deleteData($this->getGet()->getGetVar("id"));
+        $this->getSession()->createAlert("Template actually deleted !", "red");
+
+        $this->redirect("admin");
     }
 }
